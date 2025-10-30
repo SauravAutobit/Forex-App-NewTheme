53b9a9352ad296741badbbc28b2236c8a61a2a95
SauravAutobit <saurav.autobit@gmail.com>
2025-10-30 10:10:34 +0530

    feat(home): add search bar component with styling
    - Import and implement SearchBar component in Home page
    - Update primary border color in global CSS
    - Create search and microphone SVG icons
    - Style SearchBar with icons and input field

--- src/components/searchBar/SearchBar.tsx
+++ src/components/searchBar/SearchBar.tsx
@@ -1,5 +1,17 @@
+import search from "../../assets/icons/search.svg";
+import microphone from "../../assets/icons/microphone.svg";
+
 const SearchBar = () => {
-  return <div>SearchBar</div>;
+  return (
+    <div className="h-[35px] px-2 flex items-center border border-primary gap-3 rounded-10">
+      <img src={search} alt="search" />
+      <input
+        placeholder="Search"
+        className="w-full h-full border-none outline-none bg-inherit text-secondary"
+      />
+      <img src={microphone} alt="microphone" />
+    </div>
+  );
 };
 
 export default SearchBar;
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,5 +1,11 @@
+import SearchBar from "../../components/searchBar/SearchBar";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="px-5 py-2.5">
+      <SearchBar />
+    </div>
+  );
 };
 
 export default Home;
