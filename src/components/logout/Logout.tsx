10bd5aa4ae0cb4336bc9b7ea089c26b225fe3ca9
SauravAutobit <saurav.autobit@gmail.com>
2025-11-25 14:11:37 +0530

    feat(logout): add theme support to logout component
    Implement theme switching functionality in logout component by using theme selector from store. Update background and text colors based on current theme mode (dark/light).

--- src/components/logout/Logout.tsx
+++ src/components/logout/Logout.tsx
@@ -1,25 +1,30 @@
 import logoutCross from "../../assets/icons/logoutCross.svg";
+import { useAppSelector } from "../../store/hook";
 import Button from "../button/Button";
 
 const Logout = () => {
+  const theme = useAppSelector((state) => state.theme.mode);
+
   return (
-    <div className="flex flex-col gap-5 bg-[#181818] w-[319px]  rounded-10 p-2.5">
+    <div
+      className={`flex flex-col gap-5 w-[319px] rounded-10 p-2.5 ${
+        theme === "dark" ? "bg-[#181818]" : "bg-[#E5E5E5]"
+      }`}
+    >
       <div className="flex justify-end">
         <img src={logoutCross} alt="logoutCross" />
       </div>
-      <div className="font-[700] text-[17px] leading-[27px] text-center">
+      <div className="text-primary font-[700] text-[17px] leading-[27px] text-center">
         Are you sure
-        <p className="font-primary text-primary">
-          you want to end this sessions?
-        </p>
+        <p className="font-primary">you want to end this sessions?</p>
       </div>
       <div className="w-full flex items-center justify-between mt-3 mb-2.5">
         <Button
           label="Cancel"
           width="144.5px"
           height="41px"
-          bgColor="#181818"
-          textColor="#AEED09"
+          bgColor={theme === "dark" ? "#181818" : "#e5e5e5"}
+          textColor={theme === "dark" ? "#AEED09" : "#2D2D2D"}
           fontWeight={500}
         />
         <Button
@@ -29,6 +34,7 @@ const Logout = () => {
           bgColor="#AEED09"
           textColor="#2D2D2D"
           fontWeight={500}
+          boxShadow="0px 2px 4px 0px #00000040"
         />
       </div>
     </div>
